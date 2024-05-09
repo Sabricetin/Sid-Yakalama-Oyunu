77e1e8d2b39d8a22d0ca580a22a9561470509fae
Sabricetin <sabriyazilim@gmail.com>
2024-05-09 08:11:44 +0300

    Initial Commit

---
+++ Sid-yakalama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sid-yakalama
+//
+//  Created by Sabri Çetin on 9.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
